a7401dc8484ffdf7cfd3f613b4877a0be30ff317
Dirk Smits <dirksmits@gmail.com>
2017-09-01 16:43:20 +0200

    hup holland hup

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 	
-	$('.category').hide()
+/*	$('.category').hide()
 
 	$('[type="checkbox"]').change(function() {
-    	// if ( $('[type="checkbox"]').prop('checked', true) ) {
+    	if ( $('[type="checkbox"]').prop('checked', true) ) {
     		var theId = $(this).val()
     		$("#" + theId).slideToggle()
-    	/*}
+    	}
     	else {
         	$('.category').hide()
-        }*/
+        }
 	})
 
-/*	$('[type="checkbox"]').change(function() {
+	$('[type="checkbox"]').change(function() {
     	if ($('[type="checkbox"]').is(":checked")) {
         	alert("checked, unchecking");
         	$('[type="checkbox"]').prop("checked", false);
@@ -21,23 +21,30 @@ $(document).ready(function() {
         	$('[type="checkbox"]').prop("checked", true);
     	}
     })*/
-
-	$('#submit').click(function() {
-		var initialBudget = Number($("#initialBudget").val());
-		var expenses = Number($("#expenses").val());
-		var remainingBudget = initialBudget - expenses
-		var remainingBudgetPerc = 100 * (initialBudget - expenses) / initialBudget
-		console.log("your budget is now " + remainingBudget)
-		$('.statusBudget').width(remainingBudgetPerc + '%')
-	})
-
 	
+	$('#submit').click(function() {
+		var savings = Number($("#savings").val());
+		var income = Number($("#income").val());
+		var expenses = Number($("#expenses").val());
+		var initBudg = savings + income
+		var availBudg = savings + income - expenses
+		var availBudgPerc = 100 * (availBudg) / initBudg
+		console.log("savings " + savings)
+		console.log("income " + income)
+		console.log("expenses " + expenses)
+		console.log("your initial budget is now " + initBudg)
+		console.log("your budget is now " + availBudg)
+		$('.statBudgBar').width(availBudgPerc + '%')
 
-	/*$("#submit").click(function() {
-		var spent = $("#spent").val()
-		var available = $("#available").val()
-	})*/
+		if (availBudgPerc >= 90) {
+			$('.statBudgBar').css("background-color", "#b3e6b3")
+		} else if (availBudgPerc >= 10) {
+			$('.statBudgBar').css("background-color", "#ffa366")
+		} else { 
+			$('.statBudgBar').css("background-color", "#ff9999")
+		}
 
-
-
+		$('#availBudgT').text(availBudg)
+		$('#availBudgPercT').text(availBudgPerc + "%")
+	})
 })
